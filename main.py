ee4ab4d2c395bba56c56b7bc1316477805387277
Jeremy Collins <jeremy@jeremycollins.me>
2017-05-10 19:48:34 -0500

    Removed inline conditional and replaced with color function

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import pandas
 
 df = pandas.read_csv("Volcanoes-USA.txt")
 
-map=folium.Map(location=[45.372,-121.697], zoom_start=4, tiles="Stamen Terrain")
+map = folium.Map(location=[45.372,-121.697], zoom_start=4, tiles="Stamen Terrain")
 
-for lat,lon,name,elev in zip(df["LAT"],df["LON"],df["NAME"],df["ELEV"]):
-    map.add_child(folium.Marker(location=[lat,lon],popup=name, icon=folium.Icon(icon_color="green" if elev in range(0,1000) else "orange" if elev in range(1000,3000) else "red")))
+def color(elev):
+    if elev in range(0,1000):
+        col = "green"
+    elif elev in range(1000,3000):
+        col = "orange"
+    else:
+        col = "red"
+    return col
+
+for lat, lon, name, elev in zip(df["LAT"],df["LON"],df["NAME"],df["ELEV"]):
+    map.add_child(folium.Marker(location=[lat,lon],popup=name, icon=folium.Icon(icon_color=color(elev))))
 
 map.save(outfile="map.html")
 
