cc5b9c997a015f160f488f866b89f9d65a1db1f9
Jeremy Collins <jeremy@jeremycollins.me>
2017-05-10 19:33:44 -0500

    Added multi-colored markers based on elevation ranges

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ df = pandas.read_csv("Volcanoes-USA.txt")
 
 map=folium.Map(location=[45.372,-121.697], zoom_start=4, tiles="Stamen Terrain")
 
-for lat,lon,name in zip(df['LAT'],df['LON'],df['NAME']):
-    map.add_child(folium.Marker(location=[lat,lon],popup=name, icon=folium.Icon(icon_color="red")))
+for lat,lon,name,elev in zip(df["LAT"],df["LON"],df["NAME"],df["ELEV"]):
+    map.add_child(folium.Marker(location=[lat,lon],popup=name, icon=folium.Icon(icon_color="green" if elev in range(0,1000) else "orange" if elev in range(1000,3000) else "red")))
 
 map.save(outfile="map.html")
 
